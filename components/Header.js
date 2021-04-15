1dd48feea783e63bc5421edaf7bb23de61d35712
Kevin Motzkus <Kevin.Motzkus.C@gmail.com>
2021-04-15 10:00:10 +0200

    added total cost Header, Cart
    see above

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,7 @@ import {Link} from "react-router-dom"
 import {Context} from '../Context'
 
 function Header() {
-    const {cartItems} = useContext(Context)
-    console.log(cartItems.length)
+    const {cartItems, totalCost} = useContext(Context)
     return (
         <header>
             <Link to="/"><h2>Pic Some</h2></Link>
@@ -13,6 +12,7 @@ function Header() {
                 {cartItems.length > 0 ? 
                 <i className="ri-shopping-cart-fill ri-fw ri-2x"></i> : 
                 <i className="ri-shopping-cart-line ri-fw ri-2x"></i>}
+                total cost:{totalCost}
             </Link>
         </header>
     )
--- pages/Cart.js
+++ pages/Cart.js
@@ -3,16 +3,15 @@ import {Context} from "../Context"
 import CartItem from "../components/CartItem"
 
 function Cart() {
-    const {cartItems} = useContext(Context)
+    const {cartItems, totalCost} = useContext(Context)
     const cartItemElements = cartItems.map(item => (
         <CartItem key={item.id} item={item}/>
     ))
-    
     return (
         <main className="cart-page">
             <h1>Check out</h1>
             {cartItemElements}
-            <p className="total-cost">Total: </p>
+            <p className="total-cost">Total: {totalCost} </p>
             <div className="order-button">
                 <button>Place Order</button>
             </div>
